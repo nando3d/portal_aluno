a4df1ef1ac5667ccd0da62f87cf738b3a5a0d282
Fernando dos Santos Figueredo <fernandodsf@gmail.com>
2016-07-19 01:31:04 -0300

    Add files via upload
    Criacao de tabela

---
+++ tabela.sql
@@ -0,0 +1,17 @@
+/*
+-- Query: SELECT * FROM portal.livro
+LIMIT 0, 1000
+
+-- Date: 2016-07-19 01:30
+*/
+INSERT INTO `livro` (`codigo`,`titulo`,`disciplina`,`editora`,`link`) VALUES (123456,' Sistemas de Banco de Dados','Banco de Dados 2','Pearson','Download');
+INSERT INTO `livro` (`codigo`,`titulo`,`disciplina`,`editora`,`link`) VALUES (123457,'Estruturas de Dados Usando C','Estrutura de Dados','Thompson','Download');
+INSERT INTO `livro` (`codigo`,`titulo`,`disciplina`,`editora`,`link`) VALUES (123458,'Designing Interfaces','Interface Homem-Computador','O’Reilly','Download');
+INSERT INTO `livro` (`codigo`,`titulo`,`disciplina`,`editora`,`link`) VALUES (123459,'Redes de Computadores','Redes','Campus','Download');
+INSERT INTO `livro` (`codigo`,`titulo`,`disciplina`,`editora`,`link`) VALUES (123460,'Algoritmos e Lógica de Programação','Introdução a Programação','Thomson','Download');
+INSERT INTO `livro` (`codigo`,`titulo`,`disciplina`,`editora`,`link`) VALUES (123461,'Matemática Discreta para Computação e Informática','Matemática Discreta','Nacional','Download');
+INSERT INTO `livro` (`codigo`,`titulo`,`disciplina`,`editora`,`link`) VALUES (123462,'Projeto de Sistemas Com Uml','Projeto de Software','Campus','Download');
+INSERT INTO `livro` (`codigo`,`titulo`,`disciplina`,`editora`,`link`) VALUES (123463,'Utilizando Uml e Padrões','Engenharia de Requisitos','Bookman','Download');
+INSERT INTO `livro` (`codigo`,`titulo`,`disciplina`,`editora`,`link`) VALUES (123464,'Sistemas Operacionais Modernos','Sistemas Operacionais','Prentice - Hall do Brasil','Download');
+INSERT INTO `livro` (`codigo`,`titulo`,`disciplina`,`editora`,`link`) VALUES (123465,'Organização Estruturada de Computadores','Arquitetura de Computadores','Ltc','Download');
+INSERT INTO `livro` (`codigo`,`titulo`,`disciplina`,`editora`,`link`) VALUES (123466,'Engenharia de Software','Engenharia de Software','Mcgraw-hill','Download');
